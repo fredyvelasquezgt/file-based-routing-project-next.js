0eed17d6c48c1d5795238b8529d14938c3a730d2
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-25 16:40:08 -0600

    added useRouter

--- pages/clients/[id]/[clientprojectid].js
+++ pages/clients/[id]/[clientprojectid].js
@@ -1,4 +1,11 @@
+import {useRouter} from 'next/router'
+
 function SelectedClientProjectPage() {
+
+    const router = useRouter()
+    console.log(router.query)
+
+
     return <div>
         <h1>The Project Page for a specific client</h1>
     </div>
