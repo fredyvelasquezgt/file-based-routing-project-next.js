c768b831ebfa9e8f95fafb8f03eef83b0c6914ab
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-25 17:17:52 -0600

    navigating programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,6 +1,20 @@
+import {useRouter} from 'next/router'
+
 function ClientProjectsPage() {
+
+    const router = useRouter();
+    console.log(router.query)
+
+    function loadProjectHanlder() {
+        router.push({
+            pathname: '/client/[id]/[clientprojectid]',
+            query: {id: 'max', clientprojectid: 'projecta'}
+        })
+    }
+
     return <div>
         <h1>The Projects of a Given Clien</h1>
+        <button onClick={loadProjectHanlder} >Load Project A</button>
     </div>
 }
 
