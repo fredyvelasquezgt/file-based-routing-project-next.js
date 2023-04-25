0169b9084efac487ec63bf7da475ac8269e1e6c6
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-25 16:50:13 -0600

    added BlogsPostsPage

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -1,4 +1,11 @@
+import {useRouter} from 'next/router'
+ 
 function BlogsPostsPage() {
+
+    const router = useRouter();
+    console.log(router.query)
+
+
     return <div>
         <h1>The Blog Post</h1>
     </div>
