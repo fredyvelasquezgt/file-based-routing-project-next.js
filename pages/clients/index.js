3f830a1d76b0479a671ff7672e35d69f58120789
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-25 17:01:53 -0600

    added dynamic link behavior render

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,6 +1,27 @@
+import Link from 'next/link'
+
 function ClientsPage() {
+
+    const clients = [{id: 'max', name:'Maximilian'},
+                    {id: 'manu', name: 'Manuel'}]
+
     return <div>
         <h1>The Clientes Page</h1>
+        <ul>
+            <li>
+                <Link href='/clients/max'>Maximilian</Link>
+            </li>
+            <li>
+                <Link href='/clients/manu'>Manuel</Link>
+            </li>
+
+            {clients.map(client => <li key={client.id}>
+                <Link href={`/clients/${client.id}`} > 
+                {client.name}
+                </Link>
+            </li>)}
+
+        </ul>
     </div>
 }
 
