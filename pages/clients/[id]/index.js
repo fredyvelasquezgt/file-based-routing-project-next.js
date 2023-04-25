2fafc10330260e985a04433c25b688adfbf31280
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-25 16:31:09 -0600

    nested dynamic routes

---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,5 @@
+function ClientProjectsPage() {
+    return <div>
+        <h1>The Projects of a Given Clien</h1>
+    </div>
+}
---
+++ pages/clients/index.js
@@ -0,0 +1,7 @@
+function ClientsPage() {
+    return <div>
+        <h1>The Clientes Page</h1>
+    </div>
+}
+
+export default ClientsPage;
